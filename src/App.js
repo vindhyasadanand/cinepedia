9dec9d276097baf8c1e549d306d91b5694129470
Vindhya Hegde <Vindhyasadanand@gmail.com>
2021-06-03 18:36:13 +0530

    deploy my react app to github

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import SearchMovies from './SearchMovies' 
+import './App.css';
+
+function App() {
+  return (
+    
+    <div class="container">
+      <h1 className="title">React Movie Search</h1>
+      <SearchMovies/>
+    </div>
+  );
+}
+
+export default App;
